5bd0690b8e22d2f507b8555185821170d65b426b
aluno <aluno@10.61.7.17>
2026-03-12 10:10:44 -0300

    Autenticação

--- NoobStock/src/model/UsuarioDAO.java
+++ NoobStock/src/model/UsuarioDAO.java
@@ -2,12 +2,14 @@ package model;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UsuarioDAO {
 	private static final String URL = "jdbc:mysql://localhost:3306/db_noobstock";
     private static final String USUARIO = "root";
     private static final String SENHA = "sua_senha";
+	private PreparedStatement stmt;
     
     public void cadastrarUsuario(Usuario usuario) {
     	// O Controller empacota os dados soltos da tela no Modelo (Cliente)
@@ -32,5 +34,27 @@ public class UsuarioDAO {
             System.err.println("Erro ao salvar o Usuário no banco de dados: " + e.getMessage());
         }
     }
+//    public Usuario autenticar(String email, String senha) {
+//        String sql = "SELECT * FROM usuário WHERE email = ? AND senha = ?";
+//        
+//        // ... (abre a conexão e prepara o statement) ...
+//        
+//        stmt.setString(1, email);
+//        stmt.setString(2, senha);
+//        
+//        ResultSet rs = stmt.executeQuery(); // Executa a busca
+//        
+//        if (rs.next()) {
+//            // Encontrou! Pega os dados do banco e monta o objeto Cliente
+//            Usuario UsuarioLogado = new Usuario(
+//                rs.getString("nome_completo"), 
+//                rs.getString("email"),
+//                rs.getString("senha")
+//            );
+//            return UsuarioLogado; // Devolve o cliente para o Controller
+//        }
+//        
+//        return null; // Não encontrou ninguém com essa senha/email
+//    }
  
     }
