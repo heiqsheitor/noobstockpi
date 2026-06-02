d88d57b77be5fe468105141d18902472a5f52a78
aluno <aluno@10.61.7.34>
2026-06-02 10:25:09 -0300

    Set do nome na tela de inicio

--- NoobStock/src/controller/InicioController.java
+++ NoobStock/src/controller/InicioController.java
@@ -1,39 +1,42 @@
 package controller;
 
 import java.awt.event.ComponentAdapter;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.ComponentEvent; 
 
 import view.Principal;
 import view.TelaDeInicio;
 
 public class InicioController extends ComponentAdapter {
 	private TelaDeInicio view;
-    private Navegador navegador;
+	private Navegador navegador;
 
-    public InicioController(TelaDeInicio view, Navegador navegador) {
-        this.view = view;
-        this.navegador = navegador;
+	public InicioController(TelaDeInicio view, Navegador navegador) {
+		this.view = view;
+		this.navegador = navegador;
 
-        view.setInicioAcao(() -> {
-            navegador.navegarPara(Principal.INICIO);
-        });
-    
-        
-        view.setControleEstoqueAcao(() -> {
-            navegador.navegarPara(Principal.ESTOQUE);
-        });
+		view.setInicioAcao(() -> {
+			navegador.navegarPara(Principal.INICIO);
+		});
 
-        view.setPerfilAcao(() -> {
-            navegador.navegarPara(Principal.PERFIL);
-        });
-        
-        view.setFornecedorAcao(() -> {
-            navegador.navegarPara(Principal.FORNECEDOR);
-        });
+		view.setControleEstoqueAcao(() -> {
+			navegador.navegarPara(Principal.ESTOQUE);
+		});
 
-       
-    }
-	
+		view.setPerfilAcao(() -> {
+			navegador.navegarPara(Principal.PERFIL);
+		});
 
+		view.setFornecedorAcao(() -> {
+			navegador.navegarPara(Principal.FORNECEDOR);
+		});
+
+		view.addComponentListener(this);
+	}
+
+	@Override
+	public void componentShown(ComponentEvent e) {
+		if (navegador.getUsuarioLogado() != null) {
+			view.setNomeUsuario(navegador.getUsuarioLogado().getNome());
+		}
+	}
 }
